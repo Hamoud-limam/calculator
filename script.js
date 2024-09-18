87269174dcfea021304186253e8e4cf4e7424ea0
Hamoud-Limam <hamoudlimam01@gmail.com>
2024-09-18 05:52:25 -0700

    change some line in script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-
-let nums = {
+let btns = {
     num1 : document.getElementById("num1"),
     num2 : document.getElementById("num2"),
     num3 : document.getElementById("num3"),
@@ -10,16 +8,15 @@ let nums = {
     num7 : document.getElementById("num7"),
     num8 : document.getElementById("num8"),
     num9 : document.getElementById("num9"),
-    num0 : document.getElementById("num0")
-}
-
-let operations = {
+    num0 : document.getElementById("num0"),
     divise : document.getElementById("divise"),
     multiplication : document.getElementById("multiplication"),
     munis : document.getElementById("munis"),
     addition : document.getElementById("addition"),
     pourcentage : document.getElementById("pourcentage")
 }
+
+
 
 let input = document.querySelector(".view")
 let AC = document.getElementById("AC");
@@ -30,20 +27,14 @@ let equal = document.getElementById("equal") ;
 
 let inputvalue="";
 
-for(let key in nums){
-    nums[key].onclick= ()=>{
-     inputvalue +=nums[key].value
+for(let key in btns){
+    btns[key].onclick= ()=>{
+     inputvalue +=btns[key].value
      input.value=inputvalue
      console.log((inputvalue))
      }
  }
  
- for(let key in operations){
-    operations[key].onclick= ()=>{
-     inputvalue +=operations[key].value
-     input.value=inputvalue
-     }
- }
 
 AC.onclick= ()=>{
     inputvalue="";
